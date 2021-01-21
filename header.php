6d8ed084dc150274d0623772ad2c006fd0304a2a
Paolo <ptiozzo@me.com>
2021-01-21 11:45:15 +0100

    Impostazioni base

---
+++ header.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?> >
+	<head>
+		<meta charset="utf-8">
+		<title>
+			<?php
+
+			if (!is_front_page()){
+				wp_title('');
+				echo " | ";
+				bloginfo('name');
+			} else {
+				bloginfo('name');
+				echo " | ";
+				bloginfo( 'description' );
+			}
+			?>
+
+		</title>
+
+		<meta name="description" content="<?php bloginfo( 'description' ); ?>">
+		<meta name="title" content="<?php bloginfo( 'name' ); ?>">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<link rel="icon" type="image/png" href="">
+		<meta name="color-scheme" content="dark light">
+
+		<?php wp_head(); ?>
+	</head>
+  <body <?php body_class(); ?>>
